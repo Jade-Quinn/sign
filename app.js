aa3b9e187cdf3f2281e2948330b5762ecb8d175a
Jade Quinn <jadekquinn@gmail.com>
2021-05-30 13:18:40 +0000

    (feat:) Add card array to app.js

---
+++ app.js
@@ -0,0 +1,56 @@
+document.addEventListener ('DomContentLoaded', () => {
+
+    //card options
+    const cardArray = [
+        {
+            name: 'teal',
+            img: 'images/teal.jpg'
+        },
+        {
+            name: 'teal',
+            img: 'images/teal.jpg'
+        },
+        {
+            name: 'yellow',
+            img: 'images/yellow.jpg'
+        },
+        {
+            name: 'yellow',
+            img: 'images/yellow.jpg'
+        },
+        {
+            name: 'orange',
+            img: 'images/yellow.jpg'
+        },
+        {
+            name: 'orange',
+            img: 'images/yellow.jpg'
+        },
+        {
+            name: 'blue',
+            img: 'images/yellow.jpg'
+        },
+        {
+            name: 'blue',
+            img: 'images/yellow.jpg'
+        },
+        {
+            name: 'pink',
+            img: 'images/yellow.jpg'
+        },
+        {
+            name: 'pink',
+            img: 'images/yellow.jpg'
+        },
+        {
+            name: 'purple',
+            img: 'images/yellow.jpg'
+        },
+        {
+            name: 'purple',
+            img: 'images/yellow.jpg'
+        }  
+    ]
+
+    
+})
