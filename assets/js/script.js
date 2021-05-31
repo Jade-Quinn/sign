6328aee6cabb338453aa53bd6faf7453f5c42ce6
Jade Quinn <jadekquinn@gmail.com>
2021-05-31 10:11:20 +0000

    (feat:) Add event listner for document load

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//check html has loaded if true run ready function
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else  {
+    ready():
+}
+
+function ready() {
+    
+}
