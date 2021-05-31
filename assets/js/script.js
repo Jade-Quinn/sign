96d8fc49bf8c23f2da56db5605393c9224e98930
Jade Quinn <jadekquinn@gmail.com>
2021-05-31 13:09:36 +0000

    (feat:) Add audioController class and audio functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,43 @@
-// Check html has loaded if true run ready function
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', ready());
-} else  {
-    ready();
+// Create a javascript class called AudioController
+class AudioController {
+    // class requires constructor method
+    constructor() {
+        this.bgMusic = new Audio('assets/audio/creepy.mp3');
+        this.flipSound = new Audio('assets/audio/flip.wav');
+        this.matchSound = new Audio('assets/audio/match.wav');
+        this.victorySound = new Audio('assets/audio/victory.wav');
+        this.gameOverSound = new Audio('assets/audio/gameover.wav');
+        // Set background music to play at half volume
+        this.bgMusic.volume = 0.5;
+        // Set background music to repeat
+        this.bgMusic.loop = true;
+    }
+    startMusic() {
+        this.bgMusic.play();
+    }
+    stopMusic() {
+        // Pause used because there is no stop function
+        this.bgMusic.pause();
+        // Reset music to start
+        this.bgMusic.currentTime = 0;
+    }
+    flip() {
+        this.flipSound.play();
+    }
+    match() {
+        this.matchSound.play();
+    }
+    victory() {
+        this.stopMusic();
+        this.victorySound.play();
+    }
+    gameOver() {
+        this.stopMusic();
+        this.gameOverSound.play();
+    }
 }
 
+// create reday function
 function ready() {
     // Use Array.from to create an array of overlays elements and cards
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
@@ -14,6 +47,8 @@ function ready() {
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
+            // let audioController = new AudioController();
+            // audioController.startMusic();
         });
     });
 
@@ -23,4 +58,11 @@ function ready() {
             // game.flipCard(card);
         });
     });
+}
+
+// Check html has loaded if true run ready function
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else  {
+    ready();
 }
