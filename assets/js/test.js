5858462899e27dacc0331e0b69796e0533cd84a8
Jade Quinn <jadekquinn@gmail.com>
2021-06-02 05:13:19 +0000

    (feat:) Add styling to instructions on index.html

--- assets/js/test.js
+++ assets/js/test.js
@@ -116,7 +116,7 @@ class MixOrMatch {
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new MixOrMatch(60, cards);
+    let game = new MixOrMatch(80, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
