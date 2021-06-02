16d20680fdcf212dd453facd1787dc0685c024d5
Jade Quinn <jadekquinn@gmail.com>
2021-06-02 06:23:01 +0000

    (refactor:) Update sound files

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,48 +2,27 @@
 class AudioController {
     // class requires constructor method
     constructor() {
-        this.bgMusic = new Audio('assets/audio/flip.wav');
-        this.flipSound = new Audio('assets/audio/flip.wav');
-        this.matchSound = new Audio('assets/audio/match.wav');
-        this.victorySound = new Audio('assets/audio/victory.wav');
-        this.gameOverSound = new Audio('assets/audio/gameover.wav');
-        this.bgMusic.volume = 0.5; // Set background music to play at half volume
-        this.bgMusic.loop = false; // Set background music to repeat
-    }
-    startMusic() {
-        this.bgMusic.play();
-    }
-    stopMusic() {
-        this.bgMusic.pause(); // Pause used because there is no stop function
-        this.bgMusic.currentTime = 0; // Reset music to start
-    }
-    flip() {
-        this.flipSound.play();
+        this.matchSound = new Audio('assets/audio/correct.mp3');
+        this.victorySound = new Audio('assets/audio/win.wav');
     }
     match() {
         this.matchSound.play();
     }
     victory() {
-        this.stopMusic();
         this.victorySound.play();
-    }
-    gameOver() {
-        this.stopMusic();
-        this.gameOverSound.play();
     }
 }
 
 // Class to manage card interactivity
-class signGame {
+class signIt {
     constructor(totalTime, cards) {
-        this.cardsArray = cards;  // property set from constructor
+        this.cardsArray = cards; // property set from constructor
         this.totalTime = totalTime; // property set from constructor
         this.timeRemaining = totalTime;
         this.timer = document.getElementById('time-remaining'); // Get value from the DOM
         this.ticker = document.getElementById('flips'); // Get value from the DOM
         this.audioController = new AudioController();
     }
-
     startGame() {
         this.cardToCheck = null; // no card face up on game start
         this.totalClicks = 0; // set starting point clicks
@@ -51,46 +30,40 @@ class signGame {
         this.matchedCards = []; // Create empty array for matched cards to check against total cards arrary
         this.busy = true;
         setTimeout(() => { //.5 second delay
-            this.audioController.startMusic();
             this.shuffleCards();
             this.countDown = this.startCountDown(); // reset start time on new game
             this.busy = false;
-            
         }, 500);
+        this.hideCards();
         this.timer.innerText = this.timeRemaining; // reset timer when new game starts
         this.ticker.innerText = this.totalClicks;
-        this.hideCards();
     }
-
     hideCards() {
         this.cardsArray.forEach(card => {
             card.classList.remove('visible');
             card.classList.remove('matched');
         });
     }
-    
     flipCard(card) {
         if(this.canFlipCard(card)) {
-            this.audioController.flip();
             this.totalClicks++; // increment clicks
             this.ticker.innerText = this.totalClicks; // update html to show clicks number
             card.classList.add('visible');
-        }
-        if(this.cardToCheck) // if card is not null
+
+            if(this.cardToCheck) // if card is not null
                 this.checkForCardMatch(card);
             else
                 this.cardToCheck = card;
+        }
     }
-
     checkForCardMatch(card) {
         if(this.getCardType(card) === this.getCardType(this.cardToCheck)) // compare src attributes of cards
             this.cardMatch(card, this.cardToCheck);
         else 
             this.cardMisMatch(card, this.cardToCheck); // else call mismatch function
 
-            this.cardToCheck = null; // change cardToCheck back to null
+        this.cardToCheck = null; // change cardToCheck back to null
     }
-
     cardMatch(card1, card2) {
         this.matchedCards.push(card1);
         this.matchedCards.push(card2); // push both cards to the match cards array
@@ -100,35 +73,30 @@ class signGame {
         if(this.matchedCards.length === this.cardsArray.length) // if array lengths match - victory!
             this.victory(); 
     }
-
     cardMisMatch(card1, card2) {
-        this.busy = true;  // prevent users from clicking anywhere
+        this.busy = true; // prevent users from clicking anywhere
         setTimeout(() => { // delay to allow users to remember cards
             card1.classList.remove('visible');
             card2.classList.remove('visible'); // remove visible class of unmatched cards
             this.busy = false;
         }, 1000);
     }
-
     getCardType(card) {
         return card.getElementsByClassName('card-value')[0].src; // return string of src attribute on front of card
     }
-
     startCountDown() {
         return setInterval(() => { // setInterval to call function ever 1 second
             this.timeRemaining--; // increment down
             this.timer.innerText = this.timeRemaining; // update inner html
             if(this.timeRemaining === 0) // if time = 0 game over
                 this.gameOver();
-        }, 1000); //
+        }, 1000);
     }
-
     gameOver() {
         clearInterval(this.countDown); // stop timer
-        this.audioController.gameOver();
+        this.hideCards();
         document.getElementById('game-over-text').classList.add('visible');
     }
-
     victory() {
         clearInterval(this.countDown);
         this.audioController.victory();
@@ -148,14 +116,12 @@ class signGame {
     }
 }
 
-
-
 // ready function executes when DOM content is loaded
 function ready() {
     // Use Array.from to create an array of items
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new signGame(100, cards);
+    let game = new signIt(80, cards);
 
     // Loop over all overlays and add a click event listener to remove visibility
     overlays.forEach(overlay => {
@@ -164,7 +130,6 @@ function ready() {
             game.startGame();
         });
     });
-
     // Loop over all cards and add a click event listner to run flip card function
     cards.forEach(card => {
         card.addEventListener('click', () => {
@@ -174,8 +139,8 @@ function ready() {
 }
 
 // Check DOM content has loaded
-if (document.readyState === 'loading') {
+if(document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', ready());
-} else  {
+} else {
     ready();
 }
